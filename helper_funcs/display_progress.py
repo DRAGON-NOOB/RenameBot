81c635f85564f3929597bc8ea5f3354ed9aa92bf
jikkubot <66976170+jikkubot@users.noreply.github.com>
2020-08-16 00:24:32 +0400

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -47,7 +47,7 @@ async def progress_for_pyrogram(
             ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "💽 Size     :{0} of {1}\n🚀 Speed  : {2}/s\n⏱️ ETA      : {3}\n".format(
+        tmp = progress + "💽 Size     : {0} of {1}\n🚀 Speed  : {2}/s\n⏱️ ETA      : {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
